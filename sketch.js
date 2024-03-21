d52fc24f8e013efdcf71ea4b3219cd3a8f7f492d
unknown <tfig2003@gmail.com>
2024-03-21 14:43:27 +0000

    ifelse

--- sketch.js
+++ sketch.js
@@ -1,11 +1,32 @@
-function setup() {
-  createCanvas(1280, 1024);
-  background(153);
-  stroke(0);
-  strokeWeight(2);
-  fill(237, 28, 36);
-  circle(30, 30, 40);
-  line(30, 30, 1280 / 2, 1024 / 2);
-}
 
-function draw() {}
+function setup(){
+    createCanvas(400,400);
+    background(0);
+  }
+  
+  function draw(){
+    background(0);
+    stroke(255);
+    strokeWeight(3);
+  
+    if(mouseX<width/2) {
+      if(mouseY<height/2){
+        fill(255,0,0);
+      }
+      else{
+        fill(0,0,255);
+      }
+    }
+    
+    else {
+      if(mouseY<height/2){
+        fill(21,255,83);
+      }
+      else{
+        fill(255,131,0);
+      }
+    }
+    circle(width/2,height/2,height/4);
+    
+  }
+  
